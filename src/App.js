4e6d703bc16747894520ae1754d180acf108cb6a
cshimm <cameronjshimmin@gmail.com>
2024-02-21 19:24:27 -0800

    nested buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const handleOuter = () => {
+        console.log("Outer")
+    }
+    const handleInner = (e) => {
+        e.stopPropagation();
+        console.log("Inner")
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleOuter}>
+        Outer Button
+        <button onClick={handleInner}>
+            InnerButton
+        </button>
+      </button>
     </div>
   );
 }
